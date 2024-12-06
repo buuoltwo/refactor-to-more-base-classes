c9df232d82564916ad3cbc49906b723b1604a240
buuoltwo <bullnick20@gmail.com>
2024-12-06 11:41:09 +0800

    TEST2: ClassInspector.assertNoPublicFields(Cat.class, Dog.class, Boy.class, Girl.class);

--- src/main/java/com/github/hcsp/inheritance/Animal.java
+++ src/main/java/com/github/hcsp/inheritance/Animal.java
@@ -1,8 +1,8 @@
 package com.github.hcsp.inheritance;
 
 public class Animal {
-    public String name;
-    public int age;
+    private String name;
+    private int age;
 
     public Animal(String name, int age) {
         this.name = name;
